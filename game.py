7c319bae9209ba029efdbc37c61785988e9bf8c1
polyukhovich-olexandr <leanl3012@gmail.com>
2024-02-11 13:44:20 +0200

    added a function that makes the game process + launch the game.

--- game.py
+++ game.py
@@ -71,3 +71,55 @@ class Deck(object):
     def deal_card(self):
         """Функция сдачи карты"""
         return self.cards.pop()
+
+
+def new_game():
+    # Створюємо колоду
+    d = Deck()
+    # Задаємо "руки" для гравця та дилера
+    player_hand = Hand("Player")
+    dealer_hand = Hand("Dealer")
+    # Здаємо дві карти гравцю
+    player_hand.add_card(d.deal_card())
+    player_hand.add_card(d.deal_card())
+    # Здаємо одну картку дилеру
+    dealer_hand.add_card(d.deal_card())
+    print(dealer_hand)
+    print("=" * 20)
+    print(player_hand)
+    # Прапор перевірки необхідності продовжувати гру
+    in_game = True
+    # Набирати карти гравцю має сенс лише якщо у нього на руці менше 21 очка
+    while player_hand.get_value() < 21:
+        ans = input("Hit or stand? (h/s) ")
+        if ans == "h":
+            player_hand.add_card(d.deal_card())
+            print(player_hand)
+            # Якщо у гравця перебір - дилеру немає сенсу набирати карти
+            if player_hand.get_value() > 21:
+                print("You lose")
+                in_game = False
+        else:
+            print("You stand!")
+            break
+    print("=" * 20)
+    if in_game:
+        # За правилами дилер зобов'язаний набирати картки доки його рахунок менше 17
+        while dealer_hand.get_value() < 17:
+            dealer_hand.add_card(d.deal_card())
+            print(dealer_hand)
+            # Якщо у дилера перебір грати далі немає сенсу – гравець виграв
+            if dealer_hand.get_value() > 21:
+                print("Dealer bust")
+                in_game = False
+    if in_game:
+        # Ні в кого не було перебору – порівнюємо кількість очок у гравця та дилера.
+        # У нашій версії якщо у дилера та гравця рівна кількість очок – виграє казино
+        if player_hand.get_value() > dealer_hand.get_value():
+            print("You win")
+        else:
+            print("Dealer win")
+
+
+if __name__ == "__main__":
+    new_game()
